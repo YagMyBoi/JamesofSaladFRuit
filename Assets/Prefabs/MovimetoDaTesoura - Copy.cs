0b907649d90682a9225d82759abe3e127c8dafa1
João Victor Monteiro de Souza <rm91425@estudante.fieb.edu.br>
2023-07-03 18:09:33 -0300

    Versao 03/07/2023

---
+++ Assets/Prefabs/MovimetoDaTesoura - Copy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoDoLaser : MonoBehaviour
+{
+
+    public float velocLase = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+
+      transform.Translate(Vector3.up * velocLase * Time.deltaTime);
+
+      if( transform.position.y > 5.6f ){
+          Destroy(this.gameObject);
+       }
+       
+
+
+    }
+}
